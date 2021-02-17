17885b6c2b04cdfd756ef468cfafceb5bcc68c62
Adam Fowler <adamfowler71@gmail.com>
2021-02-17 18:12:56 +0000

    Got rid of default comments

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "hummingbird-websocket",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "HummingbirdWebSocket", targets: ["HummingbirdWebSocket"]),
         .library(name: "HummingbirdWSCore", targets: ["HummingbirdWSCore"]),
     ],
@@ -18,8 +17,6 @@ let package = Package(
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "HummingbirdWSCore", dependencies: [
             .product(name: "HummingbirdCore", package: "hummingbird-core"),
             .product(name: "NIO", package: "swift-nio"),
