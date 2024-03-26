9f49935e201fa87fd3f92790d8d6efa36f65d325
Adam Fowler <adamfowler71@gmail.com>
2024-03-26 14:35:08 +0000

    Wrap inbound WebSocketPacket stream and remove AsynChannel

--- Sources/HummingbirdWebSocket/WebSocketInboundStream.swift
+++ Sources/HummingbirdWebSocket/WebSocketInboundStream.swift
@@ -13,52 +13,95 @@
 //===----------------------------------------------------------------------===//
 
 import AsyncAlgorithms
+import NIOConcurrencyHelpers
 import NIOCore
 import NIOWebSocket
 
+private let webSocketPingDataSize = 16
+
 /// Inbound websocket data AsyncSequence
-public typealias WebSocketInboundStream = AsyncChannel<WebSocketDataFrame>
+// public typealias WebSocketInboundStream = AsyncChannel<WebSocketDataFrame>
 
-/// Enumeration holding WebSocket data
-public enum WebSocketDataFrame: Equatable, Sendable, CustomStringConvertible, CustomDebugStringConvertible {
-    case text(String)
-    case binary(ByteBuffer)
+public final class WebSocketInboundStream: AsyncSequence, Sendable {
+    typealias InboundIterator = NIOAsyncChannelInboundStream<WebSocketFrame>.AsyncIterator
+    let inboundIterator: UnsafeTransfer<InboundIterator>
+    let handler: WebSocketHandler
 
-    init?(frame: WebSocketFrame) {
-        switch frame.opcode {
-        case .text:
-            self = .text(String(buffer: frame.unmaskedData))
-        case .binary:
-            self = .binary(frame.unmaskedData)
-        default:
+    init(
+        inboundIterator: InboundIterator,
+        handler: WebSocketHandler
+    ) {
+        self.inboundIterator = .init(inboundIterator)
+        self.handler = handler
+    }
+
+    public struct AsyncIterator: AsyncIteratorProtocol {
+        let handler: WebSocketHandler
+        var iterator: InboundIterator
+
+        init(sequence: WebSocketInboundStream) {
+            self.handler = sequence.handler
+            self.iterator = sequence.inboundIterator.wrappedValue
+        }
+
+        public mutating func next() async throws -> WebSocketDataFrame? {
+            // parse messages coming from inbound
+            var frameSequence: WebSocketFrameSequence?
+            while let frame = try await self.iterator.next() {
+                do {
+                    self.handler.context.logger.trace("Received \(frame.opcode)")
+                    switch frame.opcode {
+                    case .connectionClose:
+                        // we received a connection close.
+                        // send a close back if it hasn't already been send and exit
+                        _ = try await self.handler.close(code: .normalClosure)
+                        return nil
+                    case .ping:
+                        try await self.handler.onPing(frame)
+                    case .pong:
+                        try await self.handler.onPong(frame)
+                    case .text, .binary:
+                        if var frameSeq = frameSequence {
+                            frameSeq.append(frame)
+                            frameSequence = frameSeq
+                        } else {
+                            frameSequence = WebSocketFrameSequence(frame: frame)
+                        }
+                    case .continuation:
+                        if var frameSeq = frameSequence {
+                            frameSeq.append(frame)
+                            frameSequence = frameSeq
+                        } else {
+                            try await self.handler.close(code: .protocolError)
+                        }
+                    default:
+                        break
+                    }
+                    if let frameSeq = frameSequence, frame.fin {
+                        var collatedFrame = frameSeq.collapsed
+                        for ext in self.handler.extensions.reversed() {
+                            collatedFrame = try await ext.processReceivedFrame(collatedFrame, context: self.handler.context)
+                        }
+                        if let finalFrame = WebSocketDataFrame(frame: collatedFrame) {
+                            frameSequence = nil
+                            return finalFrame
+                        }
+                    }
+                } catch {
+                    // catch errors while processing websocket frames so responding close message
+                    // can be dealt with
+                    let errorCode = WebSocketErrorCode(error)
+                    try await self.handler.close(code: errorCode)
+                }
+            }
+
             return nil
         }
     }
 
-    public var webSocketFrame: WebSocketFrame {
-        switch self {
-        case .text(let string):
-            return .init(fin: true, opcode: .text, data: ByteBuffer(string: string))
-        case .binary(let buffer):
-            return .init(fin: true, opcode: .binary, data: buffer)
-        }
-    }
+    public typealias Element = WebSocketDataFrame
 
-    public var description: String {
-        switch self {
-        case .text(let string):
-            return "string(\"\(string)\")"
-        case .binary(let buffer):
-            return "binary(\(buffer.description))"
-        }
-    }
-
-    public var debugDescription: String {
-        switch self {
-        case .text(let string):
-            return "string(\"\(string)\")"
-        case .binary(let buffer):
-            return "binary(\(buffer.debugDescription))"
-        }
+    public func makeAsyncIterator() -> AsyncIterator {
+        .init(sequence: self)
     }
 }
