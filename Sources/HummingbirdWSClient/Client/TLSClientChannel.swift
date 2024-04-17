505d324929ab5020b65923ca4d44c3a755efe6d9
Adam Fowler <adamfowler71@gmail.com>
2024-04-17 09:41:10 +0100

    Add min frame to compress config/fix closing bug (#57)
    * Add minFrameSizeToCompress configuration
    
    * Change closed boolean to CloseState
    
    Fixes an issue where the handler would wait for a close frame even though it has already received one
    
    * Swift format

--- Sources/HummingbirdWSClient/Client/TLSClientChannel.swift
+++ Sources/HummingbirdWSClient/Client/TLSClientChannel.swift
@@ -17,6 +17,7 @@ import NIOCore
 import NIOSSL
 
 /// Sets up client channel to use TLS before accessing base channel setup
+@_documentation(visibility: internal)
 public struct TLSClientChannel<BaseChannel: ClientConnectionChannel>: ClientConnectionChannel {
     public typealias Value = BaseChannel.Value
 
